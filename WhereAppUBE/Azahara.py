2c37068308d950189c9c97f44824a81724a2adf3
JorgeGarciaIrazbal <jorge.girazabal@gmail.com>
2015-06-07 17:56:14 +0200

    refreshing project

--- WhereAppUBE/Azahara.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-
-while(True):
-    respuesta= raw_input("hemos perdido o hemos gaado")
-
-
-    litrosPerdidos = int(raw_input("cuantos litros hemos perdido"))
-    litrosGanados = int(raw_input("cuantos litros hemos ganado"))
-
